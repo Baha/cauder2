3c6888d974de62a051fc5970b47e0fc309c24df4
Adrian Palacios <ukonat@gmail.com>
2018-03-23 14:47:13 +0100

    Multiple evals passing env around

--- src/rev_eval.erl
+++ src/rev_eval.erl
@@ -1,20 +1,26 @@
 -module(rev_eval).
 
--export([eval/1]).
+-export([eval/1,expr/2, exprs/1]).
 
 eval(StrExp) ->
-  {ok, ParsedExp, _} = erl_scan:string(StrExp),
-  {ok, [TypedExp]} = erl_parse:parse_exprs(ParsedExp),
-  eval:expr(TypedExp, orddict:new()).
+  {ok, ParsedExps, _} = erl_scan:string(StrExp),
+  {ok, TypedExps} = erl_parse:parse_exprs(ParsedExps),
+  %eval:expr(TypedExp, orddict:new()).
+  EvPid = spawn(rev_eval, exprs, [TypedExps]),
+  register(ev, EvPid).
 
-% expr(E, Bs) ->
-%   RefId = make_ref()
-%   tracer ! {RefId, E, Bs},
-%   Res =
-%     receive
-%       continue ->
-%         eval:expr(E, Bs);
-%       {continue, Val} ->
-%         Val
-%     end,
-%   Res.
+exprs(Es) ->
+  F = fun (E, Bs) -> expr(E,Bs) end,
+  lists:foldl(F, orddict:new(), Es).
+
+expr(E, Bs) ->
+  % RefId = make_ref(),
+  % tracer ! {RefId, E, Bs},
+  io:format("~p ~p~n",[E, Bs]),
+  receive
+    cont ->
+      case (catch eval:expr(E, Bs)) of
+        {value, NE, NBs} -> expr(NE, NBs);
+        _ -> Bs
+      end
+  end.
